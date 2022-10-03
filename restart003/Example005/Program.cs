e93c9bf542976128093235d2c7e8168c61b6073a
Alex <alexey.chuprov.x@gmail.com>
2022-10-03 19:56:57 +0300

    cool

---
+++ restart003/Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "evlyv")
+{
+    Console.WriteLine("Привет, я люблю тебя, evlyv!!!");
+}
+else
+{
+    Console.Write("Привет, мой маленький ублюдок.");
+    Console.Write(username);
+}
