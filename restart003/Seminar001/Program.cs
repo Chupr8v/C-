422431f8e5df51c980be709bc3629bd78181f3fb
Alex <alexey.chuprov.x@gmail.com>
2022-10-04 21:00:56 +0300

    Seminar001

---
+++ restart003/Seminar001/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int b = 7;
+int c = 23;
+
+if(a % c == 0)
+{
+    Console.WriteLine (" Кратное 7 ");
+} 
+else
+{
+    Console.WriteLine (" Некратно 7 ");
+}
+
+if(a % b == 0)
+{
+    Console.Writeline (" Кратное 23 ");
+}
+
+else
+{
+    Console.WriteLine (" Некратно 23 ");
+}
+
