749918f4c832c4de900b677139f7831b68d93d3b
Alex <alexey.chuprov.x@gmail.com>
2022-10-04 22:08:00 +0300

    Seminar003

---
+++ restart003/Seminar003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int a = int.Parse(Console.ReadLine() ?? "0");
+string b = a.ToString();
+if(b[0] == b[4] && b[1] == b[3])
+{
+    Console.WriteLine("Палендром");
+}
+else
+{
+    Console.WriteLine("Не полендром");
+}
