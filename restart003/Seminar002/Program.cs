9da0e9e735edc746d1b7ddf5af0cf7bdf0eabf9e
Alex <alexey.chuprov.x@gmail.com>
2022-10-04 22:08:18 +0300

    Seminar002

---
+++ restart003/Seminar002/Program.cs
@@ -0,0 +1,29 @@
+﻿//Console.WriteLine("Введите число");
+//double b = double.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите x");
+int x = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите y");
+int y = int.Parse(Console.ReadLine() ?? "0");
+
+if ((x > 0) && (y > 0))
+{
+    Console.Write("1-я четверть");
+}
+
+else if ((x > 0) && (y < 0))
+{
+    Console.Write("2-я четверть");
+}
+
+else if ((x < 0) && (y < 0))
+{
+    Console.Write("3-я четверть");
+}
+
+else if ((x < 0) && (y > 0))
+{
+    Console.Write("4-я четверть");
+}
+
