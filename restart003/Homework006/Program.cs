3aa4fad15b272067223dddf1c47d794e07daf995
Alex <alexey.chuprov.x@gmail.com>
2022-10-07 19:07:39 +0300

    Homework006

---
+++ restart003/Homework006/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать последнюю цифру трёхзначного числа
+Console.Write("Введите трёхзначное число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+string b = a.ToString();
+Console.Write("Третья цифра: ");
+Console.WriteLine(b[2]);
