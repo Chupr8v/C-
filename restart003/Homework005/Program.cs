9e6227a8e6425e3cb549d584cb05904e0172e3cc
Alex <alexey.chuprov.x@gmail.com>
2022-10-07 18:56:08 +0300

    Homework005

---
+++ restart003/Homework005/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать четные числа от 1 до N
+Console.Write("Введите целое число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int number = 2;
+
+while(number < n)
+{
+    if(number % 2 == 0)
+    {
+        Console.WriteLine(number);
+    }
+    number = number + 2;
+}
+
+
+
+
