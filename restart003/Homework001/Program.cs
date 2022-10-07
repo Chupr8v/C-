d50038aeeb8ebd2e485faaab68b2c4a449dda070
Alex <alexey.chuprov.x@gmail.com>
2022-10-07 16:43:25 +0300

    Homework001

---
+++ restart003/Homework001/Program.cs
@@ -0,0 +1,14 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Введите a: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите b: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+if(a * a == b)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("No"); 
+}
