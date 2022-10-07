44acb715d27917170f2db0fd5105d921f6ddb7ae
Alex <alexey.chuprov.x@gmail.com>
2022-10-07 19:29:29 +0300

    Homework008

---
+++ restart003/Homework008/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+Console.Write("Введите число от 10 до 99: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+string b = a.ToString();
+
+if(b[1] > b[0])
+{   
+    Console.Write("Наибольшая цифра: ");
+    Console.WriteLine(b[1]);
+}    
+if(b[0] > b[1])
+{
+    Console.Write("Наибольшая цифра: ");
+    Console.WriteLine(b[0]);
+}    
