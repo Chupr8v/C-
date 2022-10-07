7b759d5ca733180551403450d473c90b729ab415
Alex <alexey.chuprov.x@gmail.com>
2022-10-07 19:13:30 +0300

    Homework007

---
+++ restart003/Homework007/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.Write("Введите трёхзначное число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+string b = a.ToString();
+Console.Write("Второе цифра: ");
+Console.WriteLine(b[1]);
