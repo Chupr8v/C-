3f4480466a06bd3b2f0ae892fc3bd2858e07e0f1
Alex <alexey.chuprov.x@gmail.com>
2022-10-07 18:55:56 +0300

    Homework004

---
+++ restart003/Homework004/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+Console.Write("Введите целое число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+     Console.WriteLine("Число нечётное");
+}
+
