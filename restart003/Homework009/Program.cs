7d0a080643a8b6dc58f0938f2e322804b4d648fb
Alex <alexey.chuprov.x@gmail.com>
2022-10-07 19:41:35 +0300

    Homework009

---
+++ restart003/Homework009/Program.cs
@@ -0,0 +1,10 @@
+﻿// Удалить вторую цифру трёхзначного числа
+//H010 Выяснить, кратно ли число заданному, если нет, вывести остаток.
+//H011 Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("Введите трёхзначное число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+string b = a.ToString();
+Console.WriteLine(b);
+Console.Write(b[0]);
+Console.WriteLine(b[2]);
