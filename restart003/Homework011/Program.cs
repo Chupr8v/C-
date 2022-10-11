b1a95de542891f22ed4f1dde2dc8788b8151b976
Alex <alexey.chuprov.x@gmail.com>
2022-10-11 18:12:27 +0300

    Homework011

---
+++ restart003/Homework011/Program.cs
@@ -0,0 +1,34 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.Write("Введите номер дня недели: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+if(username.ToLower() == "2")
+{
+    Console.WriteLine("Вторник");
+}
+if(username.ToLower() == "3")
+{
+    Console.WriteLine("Среда");
+}
+if(username.ToLower() == "4")
+{
+    Console.WriteLine("Четверг");
+}
+if(username.ToLower() == "5")
+{
+    Console.WriteLine("Пятница - улица развратница");
+}
+if(username.ToLower() == "6")
+{
+    Console.WriteLine("Суббота - выходной!");
+}
+if(username.ToLower() == "7")
+{
+    Console.WriteLine("Воскресенье - выходной!");
+}
+
+
