7c585effcd911cdf5a294551ca0cf188feaf1750
Alex <alexey.chuprov.x@gmail.com>
2022-10-16 02:29:37 +0300

    H014

--- restart003/Homework014/Program.cs
+++ restart003/Homework014/Program.cs
@@ -6,3 +6,22 @@
 
     d = Math.Sqrt(x3 * x3 + y3 * y3 + z3 * z3);
 */
+
+Console.Write("Введите координаты xa: "); 
+int xa = int.Parse(Console.ReadLine() ?? "0"); 
+Console.Write("Введите координаты ya: "); 
+int ya = int.Parse(Console.ReadLine() ?? "0"); 
+Console.Write("Введите координаты za: "); 
+int za = int.Parse(Console.ReadLine() ?? "0"); 
+
+Console.Write("Введите координаты xb: "); 
+int xb = int.Parse(Console.ReadLine() ?? "0"); 
+Console.Write("Введите координаты yb: "); 
+int yb = int.Parse(Console.ReadLine() ?? "0"); 
+Console.Write("Введите координаты zb: "); 
+int zb = int.Parse(Console.ReadLine() ?? "0"); 
+
+double d = Math.Sqrt((xb - xa)*(xb-xa)+(yb-ya)*(yb-ya)+(zb-za)*(zb-za));
+Console.WriteLine($"Расстояние между точкой a с координатами {xa};{ya};{za} и точкой b с координатами {xb};{yb};{zb} равно {d}.");
+
+
