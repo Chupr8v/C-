c579fe728f2ab9a48476b0311a5e63e1116b1560
Alex <alexey.chuprov.x@gmail.com>
2022-10-17 00:08:25 +0300

    Homework016

---
+++ restart003/Homework016/Program.cs
@@ -0,0 +1,12 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Write("Введите число n: "); 
+int n = int.Parse(Console.ReadLine() ?? "0"); 
+string b = n.ToString();
+int sum = 0;
+for(int i = 0; i != b.Length; i++)
+{
+    sum = sum + (b[i] - '0');
+
+}
+
+Console.WriteLine($"Сумма чисел числа {n} = {sum}");
