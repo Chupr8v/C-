d59cccf5e03fede11ec35127bbc6c1076154a534
Alex <alexey.chuprov.x@gmail.com>
2022-10-17 00:20:40 +0300

    Homework018

---
+++ restart003/Homework018/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("Введите число n: "); 
+int n = int.Parse(Console.ReadLine() ?? "0"); 
+int s = 1;
+for(int a = 1; a<=n; a++)
+{
+    s=a*a*a;
+    if(s % 2 == 0)
+    {
+        Console.WriteLine($"{a} в степени 3 = {s}");
+    }    
+}
