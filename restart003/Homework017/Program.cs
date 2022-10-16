44bfa1ba9b41c885ccd2b09a1bca5e2c8411f2d8
Alex <alexey.chuprov.x@gmail.com>
2022-10-17 00:08:39 +0300

    Homework017

---
+++ restart003/Homework017/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Write("Введите число n: "); 
+double n = double.Parse(Console.ReadLine() ?? "0"); 
+double a = 1;
+for(double sum = 1; a<=n; a++)
+{
+    Console.Write($"{sum} * {a} = ");
+    sum = sum * a;
+    Console.WriteLine(sum);
+}
