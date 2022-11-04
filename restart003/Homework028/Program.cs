bf8c7bf01ae23691e5a72266fbf0bb88fc45f26a
Alex <alexey.chuprov.x@gmail.com>
2022-11-04 17:04:07 +0300

    H028

---
+++ restart003/Homework028/Program.cs
@@ -0,0 +1,19 @@
+﻿// Показать числа Фибоначчи
+Console.WriteLine("До какого числа считать ряд Фибоначчи?");
+int number = Convert.ToInt32(Console.ReadLine());
+int perv = 1;
+Console.Write("{0} ", perv);
+int vtor = 1;
+Console.Write("{0} ", vtor);
+int sum = 0;
+
+while (number >= sum)
+            {
+                sum = perv + vtor;
+                
+                    Console.Write("{0} ", sum);
+                    {
+                        perv = vtor;
+                        vtor = sum;                
+                    }            
+            }
