b16b4ea0280c3e38a388c0981a45369a73883e03
Alex <alexey.chuprov.x@gmail.com>
2022-11-27 11:09:09 +0300

    2

---
+++ restart003/fin.2Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumNumbers(int M, int N)
+{
+    if (M == 0) return (N * (N + 1)) / 2;            // Если M равно нулю
+            else if (N == 0) return (M * (M + 1)) / 2;       // Если N равно нулю
+            else if (M == N) return M;                       // Если M=N
+            else if (M < N) return N + SumNumbers(M, N - 1); // Если M<N
+            else return N + SumNumbers(M, N + 1);            // Если M>N
+}
+
+Console.WriteLine(SumNumbers(2, 11));
