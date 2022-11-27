3a3e8bbc7000b0be0d9b2391cc091e89c283f090
Alex <alexey.chuprov.x@gmail.com>
2022-11-27 11:08:56 +0300

    1

---
+++ restart003/fin.1Homework/Program.cs
@@ -0,0 +1,7 @@
+﻿//Показать натуральные числа от M до N, N и M заданы
+string NumbersRec(int m, int n)
+{
+    if(m<=n) return $"{m} " + NumbersRec(m+1, n);
+    else return String.Empty;
+}
+Console.WriteLine(NumbersRec(1, 10));         // (a+b)*(b-a+1)/2
