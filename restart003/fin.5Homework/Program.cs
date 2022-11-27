120fe522c56ac473ac336e2be18b599330125202
Alex <alexey.chuprov.x@gmail.com>
2022-11-27 11:09:45 +0300

    5

---
+++ restart003/fin.5Homework/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+// Первые два элемента последовательности задаются пользователем
+Console.WriteLine("До какого числа считать ряд Фибоначчи?");
+int number = Convert.ToInt32(Console.ReadLine());
+int perv = 1;
+Console.Write("{0} ", perv);
+int vtor = 2;
+Console.Write("{0} ", vtor);
+int sum = 0;
+
+while (number >= sum)
+            {
+                sum = perv + vtor;
+                
+                    Console.Write("{0} ", sum);
+                    {
+                        perv = vtor;
+                        vtor = sum;                
+                    }            
+            }
